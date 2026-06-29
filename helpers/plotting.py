118623943fe38a4b4e7f679500b214b0b5d7cdff
Vardhan-R <vrdhnr@gmail.com>
2026-06-30 04:26:49 +0530

    Changed uuid7 to uuid4

--- helpers/plotting.py
+++ helpers/plotting.py
@@ -1,4 +1,4 @@
-from uuid import uuid7
+from uuid import uuid4
 import altair as alt
 import pandas as pd
 import numpy as np
@@ -6,9 +6,9 @@ import numpy as np
 
 def plotLineGraph(x: np.ndarray, y: np.ndarray, colour: str, x_label: str = "x", y_label: str = "y") -> alt.Chart:
     source = pd.DataFrame({x_label: x, y_label: y})
-    return alt.Chart(source).mark_line(color=colour).encode(x=x_label, y=y_label).interactive(name=str(uuid7()))
+    return alt.Chart(source).mark_line(color=colour).encode(x=x_label, y=y_label).interactive(name=str(uuid4()))
 
 
 def plotScatterGraph(x: np.ndarray, y: np.ndarray, colour: str, x_label: str = "x", y_label: str = "y") -> alt.Chart:
     source = pd.DataFrame({x_label: x, y_label: y})
-    return alt.Chart(source).mark_circle(color=colour, size=60).encode(x=x_label, y=y_label).interactive(name=str(uuid7()))
+    return alt.Chart(source).mark_circle(color=colour, size=60).encode(x=x_label, y=y_label).interactive(name=str(uuid4()))
--- modules/auth_manager.py
+++ modules/auth_manager.py
@@ -147,8 +147,6 @@ def load_user(username: str, password: str) -> bool:
     else:
         try:
             conn = st.connection("supabase", type=SupabaseConnection)
-
-            st.text(conn.table("users").select("*").execute())
             result = conn.table("users").select("password_hash").eq("username", username).execute()
             if not result.data:
                 print("Username not found.")
